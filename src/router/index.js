e9a3b21d82a52e119aef17fe6b607c7464d1126f
lele <602300579@qq.com>
2019-10-24 15:25:37 +0800

    我修改了页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
-import Index from '../components/index.vue'
+import Index from '../components/Index.vue'
 
 Vue.use(VueRouter)
 
